fe7b6663507388f32fad59dc5ef681ca6aa98f7c
vmmuthu31 <mvairamuthu2003@gmail.com>
2023-10-20 09:19:32 +0530

    updated the pages with the forms

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import HomeNav from './Components/HomeNav'
+import Link from 'next/link'
 
 function Home() {
   return (
@@ -7,19 +8,22 @@ function Home() {
         <HomeNav />
         <div className='py-16'>
             <p className='text-center text-4xl font-bold text-white'>Choose Your Role</p>
-            <div className='flex mx-20  justify-center my-14 space-x-10'>
-            <div className='bg-[#1C1D2E] hover:bg-orange-400 text-white cursor-pointer hover:text-gray-800 rounded-xl  w-[400px]'>
-                <div className='flex rounded-lg my-10  flex-col space-y-5 py-5 mx-4'>
+            <div className='flex mx-20  justify-center my-10 space-x-10'>
+          <Link href="/SendRewards">
+              <div className='bg-[#1C1D2E] h-[250px] rounded-xl hover:bg-orange-400 cursor-pointer text-white hover:text-gray-800  w-[400px]'>
+                <div className='flex rounded-lg my-10  flex-col space-y-5 py-10 mx-4'>
                    <p className='text-4xl font-bold text-center  '>Send Rewards</p>
                    <p className='text-xl   px-5 text-justify'>Send tokens effortlessly to your chosen recipients as a token of your appreciation, in just a few clicks.</p>
                 </div>
             </div>
-            <div className='bg-[#1C1D2E] rounded-xl hover:bg-orange-400 cursor-pointer text-white hover:text-gray-800  w-[400px]'>
-                <div className='flex rounded-lg my-10  flex-col space-y-5 py-5 mx-4'>
+            </Link>
+            <Link href="/ClaimRewards">   <div className='bg-[#1C1D2E] rounded-xl hover:bg-orange-400 cursor-pointer text-white hover:text-gray-800  w-[400px]'>
+                <div className='flex rounded-lg my-10  flex-col space-y-5 py-10 mx-4'>
                    <p className='text-4xl font-bold text-center '>Claim Rewards</p>
                    <p className='text-xl  text-justify px-5 '>Claim tokens seamlessly by entering a code or link, turning your rewards into real value with simplicity and speed.</p>
                 </div>
             </div>
+            </Link>
             </div>
         </div>
     </div>
--- pages/SendRewards.tsx
+++ pages/SendRewards.tsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import Navbar from './Components/Navbar'
+import { useRouter } from 'next/router'
 
 function SendRewards() {
+  const router = useRouter()
+
+    function Submit(){
+      router.push("/Home")
+    }
   return (
-    <div>SendRewards</div>
+    <div className="bg-[#090022] min-h-screen ">
+        <Navbar />
+        <div className='py-16'>
+            <p className='text-center text-3xl font-bold text-white'>Registration</p>
+            <div className='bg-[#1C1D2E] rounded-xl mx-96'>
+                <div className='flex rounded-lg my-10  flex-col space-y-10 py-20 mx-40'>
+                    <div className='flex flex-col '>
+                   <span className='px-6'> <label className='text-white absolute  top-[295px]  bg-[#1C1D2E] px-2 font-semibold' >Name</label></span>
+                    <input className='h-10 text-white rounded-lg bg-transparent border border-gray-300 border-opacity-90 px-2' type="text" />
+                    </div>
+                    <div className='flex flex-col'>
+                    <span className='px-6'> <label className='text-white absolute  top-[375px] bg-[#1C1D2E] font-semibold px-2' >Email</label></span>
+                    <input className='h-10 text-white rounded-lg bg-transparent border border-gray-300 border-opacity-90  px-2' type="email"/>
+                    
+                    </div>
+                    <button onClick={Submit} className="bg-[#FF6B00] px-3 py-2 rounded-lg text-white font-semibold text-xl">Submit</button>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
