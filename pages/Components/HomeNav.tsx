992886dca6be9aa7a293909d07d8f175e561ecaa
vmmuthu31 <mvairamuthu2003@gmail.com>
2023-10-20 08:35:02 +0530

    added the home navbar

--- pages/Components/HomeNav.tsx
+++ pages/Components/HomeNav.tsx
@@ -5,8 +5,13 @@ import React from 'react'
 function HomeNav() {
   return (
     <div>
-        <div className='flex  justify-between py-5  px-20  '>
+        <div className='flex  justify-between py-10  px-20  '>
             <Link href="/"> <img src="https://blogger.googleusercontent.com/img/a/AVvXsEi0Mf1wBXxzRyBZX_6L1dk0eSmpupougjaSWD8WEvhLWx1YepcL4--j9CN56ZS3IJ3t6iQZVObl0PuQuAIR4YbM5osTo0NmAap67aifxKgUGxaboLk7d9Z-7flM3FxKBhypS4cKZqGkGMXY4Ty-_kI8zh3lQztLlGxtk5u1FbIQqiAznXAfFKzO8Gvwch0"className='h-12 w-auto' /></Link>
+            <div className='flex text-xl text-white font-semibold space-x-6 pt-3'>
+              <p>Home</p>
+              <p>Send Rewards</p>
+              <p>Claim Rewards</p>
+            </div>
             <ConnectButton />
         </div>
     </div>
--- pages/Components/Navbar.tsx
+++ pages/Components/Navbar.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 function Navbar() {
   return (
     <div>
-        <div className='flex  justify-between py-5  px-20  '>
+        <div className='flex  justify-between pt-10  px-20  '>
             <Link href="/"> <img src="https://blogger.googleusercontent.com/img/a/AVvXsEi0Mf1wBXxzRyBZX_6L1dk0eSmpupougjaSWD8WEvhLWx1YepcL4--j9CN56ZS3IJ3t6iQZVObl0PuQuAIR4YbM5osTo0NmAap67aifxKgUGxaboLk7d9Z-7flM3FxKBhypS4cKZqGkGMXY4Ty-_kI8zh3lQztLlGxtk5u1FbIQqiAznXAfFKzO8Gvwch0"className='h-12 w-auto' /></Link>
             <ConnectButton />
         </div>
