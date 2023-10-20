a7d777de3b35a8f1f00ee6b520a512870604aaeb
vmmuthu31 <mvairamuthu2003@gmail.com>
2023-10-20 09:12:45 +0530

    added the function for those two pages

---
+++ pages/ClaimRewards.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ClaimRewards() {
+  return (
+    <div>ClaimRewards</div>
+  )
+}
+
+export default ClaimRewards
---
+++ pages/SendRewards.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SendRewards() {
+  return (
+    <div>SendRewards</div>
+  )
+}
+
+export default SendRewards
